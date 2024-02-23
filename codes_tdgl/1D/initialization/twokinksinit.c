d8951bb091a3783f5dccace089dbd10c3129af68
suanno <xroccosuannox@gmail.com>
2024-02-23 01:36:11 +0100

    The time dependance of f'(t) is not so irrelevant!

---
+++ codes_tdgl/1D/initialization/twokinksinit.c
@@ -0,0 +1,70 @@
+//#include <iostream.h>
+//#include <fstream.h>
+#include <string.h>
+#include <stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<omp.h>
+#include <time.h>
+
+#define pi  4*atan(1.0)
+
+//pour générer des nombres aléatoires
+double randU(double randmin, double randmax)
+{
+double randU1=0.;
+randU1 = randmin*(1-rand()/(double)RAND_MAX)+randmax*rand()/(double)RAND_MAX;
+return randU1;
+}
+
+double doublekink(double x, double xc, double h){
+    return tanh(x-xc+h/2)-tanh(x-xc-h/2)-1;
+}
+
+int main(int argc, char  *argv [ ]){
+
+int i;
+
+int N=1000;
+double u0;              /*Initial flat value*/
+int seed;
+double hmoy = 0;
+
+
+seed = time(NULL);
+srand(seed);
+
+char *ptr;
+if (argc > 1)
+	N = (int)strtod(argv[1], &ptr);
+if (argc > 2){
+  	u0 = strtod(argv[2], &ptr);
+} else{
+    u0=randU(1, 1)+hmoy;
+}
+
+
+
+FILE *fileinit;
+
+fileinit = fopen("fileinit.dat", "w");
+fprintf(fileinit, "%d %d\n", seed, N);
+#pragma omp parallel for
+for (i=0; i<N; i++){
+    fprintf(fileinit, "%.20f\n", doublekink(i, N/2, N/5));
+}
+
+fclose(fileinit);
+
+/*Recreate fileCout of values of C(t) [Progressive
+executions of the dynamics will APPEND info]*/
+FILE *file;
+file = fopen("fileCout.dat", "w");
+fclose(file);
+file = fopen("fileAveout.dat", "w");
+fclose(file);
+
+
+return 0;
+
+}
